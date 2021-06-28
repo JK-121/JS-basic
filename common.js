50651da02b9fe9d588dea881f7ef9a9a2cf5c2ef
JK-121 <kyeongmin719@gmail.com>
2021-06-28 13:25:33 +0900

    JS 함수 파일로 만들기

---
+++ common.js
@@ -0,0 +1,23 @@
+function getAgeGroup(users, keyAge) {
+    let userAgeGroup = {};
+
+    for (var user of userList) {
+        var ageGroup = "";
+        if (user[KeyAge] < 10) {
+            ageGroup = "0";
+        } else if (user[keyAge] >= 100) {
+            ageGroup = user[keyAge].toString().substr(0, 2) + "0";
+        } else {
+            ageGroup = user[keyAge].toString().charAt(0) + "0";
+        }
+
+        if (userAgeGroup[ageGroup]) {
+            userAgeGroup[ageGroup = userAgeGroup[ageGroup]] + 1;
+        } else {
+            userAgeGroup[ageGroup] = 1;
+        }
+
+    }
+
+    return userAgeGroup;
+}
