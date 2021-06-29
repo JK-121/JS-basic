d07426977d58256a9f857077c8f63a0e221f4add
JK-121 <kyeongmin719@gmail.com>
2021-06-29 10:46:56 +0900

    JS Object Litteral Syntax Extension

---
+++ 040.js
@@ -0,0 +1,16 @@
+//Object Litteral Syntax Extension
+
+var firstName = "John";
+var lastName = "Bur";
+
+var person = {
+    firstName: firstName,
+    lastName: lastName
+}
+
+var type = "student";
+var scope = {
+    [type]: "John Bur",
+    //student: "John Bur",
+    score: 95
+};
