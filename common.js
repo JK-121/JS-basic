6e5c2c7c262e43b55d8cba074ea9e992ab4b6830
JK-121 <kyeongmin719@gmail.com>
2021-06-29 09:46:13 +0900

    JS 함수 모음

--- common.js
+++ common.js
@@ -22,6 +22,12 @@ function getAgeGroup(users, keyAge) {
     return userAgeGroup;
 }
 
+/**
+ * 최소값과 최대값을 파라미터로 받고 그 사이의 무작위 정수값 반환.
+ * @param {*} min 
+ * @param {*} max 
+ * @returns 
+ */
 function getRandomInterger(min, max) {
     return Math.trunc(Math.random() * (max - min + 1)) + min;
 }
