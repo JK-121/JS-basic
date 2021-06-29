ed1ce59742087cea90a3258777f63e5fa5556077
JK-121 <kyeongmin719@gmail.com>
2021-06-29 10:40:03 +0900

    JS Arrow

---
+++ 038.js
@@ -0,0 +1,20 @@
+//Arrow Function
+
+//함수 표현식
+//var add = funciton(x1, x2){
+//     return x1 + x2;
+// }
+
+var add = (x1, x2) => x1 + x2;
+
+function say(message) {
+
+}
+
+var say = (message) => {};
+
+function myFunction() {
+
+}
+
+var myFunction = () => {};
