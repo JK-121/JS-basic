cebd2e8370ab959b6a53ae456cc7e1ccf3c1dee8
JK-121 <kyeongmin719@gmail.com>
2021-06-30 09:12:09 +0900

    JS Array Destructuring

--- 043.js
+++ 043.js
@@ -4,10 +4,10 @@ function getScores() {
     return [70, 21, 100];
 }
 
-var scores = getScores();
-scores[0];
-scores[1];
-scores[2];
+// var scores = getScores();
+// scores[0];
+// scores[1];
+// scores[2];
 
 var [x, y, z] = getScores();
 console.log(x);
